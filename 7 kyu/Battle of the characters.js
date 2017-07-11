3ffe41098246bf8d75d21a5c66e5ca7ec1e87667
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-07-12 02:50:52 +0530

    second week

--- 7 kyu/Battle of the characters.js
+++ 7 kyu/Battle of the characters.js
@@ -1,42 +1,15 @@
-function findSumOfWord ( word ) {
-    var letterValues = {
-      A: 1,
-      B: 2,
-      C: 3,
-      D: 4,
-      E: 5,
-      F: 6,
-      G: 7,
-      H: 8,
-      I: 9,
-      J: 10,
-      K: 11,
-      L: 12,
-      M: 13,
-      N: 14,
-      O: 15,
-      P: 16,
-      Q: 17,
-      R: 18,
-      S: 19,
-      T: 20,
-      U: 21,
-      V: 22,
-      W: 23,
-      X: 24,
-      Y: 25,
-      Z: 26
+function battle(x, y) {
+  var power = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+  var sumx = 0;
+  var sumy = 0;
 
-    };
+  x.split("").forEach(function (letter) {
+    sumx += power.indexOf(letter) + 1;
+  });
 
-    return word.split("").reduce( function ( sum, value ) {
-        return letterValues[value] + sum;
-    }, 0 );
-}
+  y.split("").forEach(function (letter) {
+    sumy += power.indexOf(letter) + 1;
+  });
 
-function battle ( word1, word2 ) {
-    var sumOfWord1 = findSumOfWord( word1 );
-    var sumOfWord2 = findSumOfWord( word2 );
-
-    return sumOfWord1 > sumOfWord2 ? word1 : ( sumOfWord1 === sumOfWord2 ? "Tie!" : word2 );
+  return (sumx > sumy) ? x : ((sumy > sumx) ? y : "Tie!");
 }
