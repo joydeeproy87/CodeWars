9e27e7eee88f589f6cc5f50ed0e2c377d3490efb
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-07-14 17:07:46 +0530

    second week

--- 5 kyu/Sum of Pairs.js
+++ 5 kyu/Sum of Pairs.js
@@ -1,26 +1,24 @@
-var sum_pairs = function (ints, s) {
-    var film = [];
-    var pair = [];
-  	var holder;
+var sum_pairs = function sum_pairs(ints, s) {
+  var film = [];
+  var pair = [];
+  var holder;
 
-    for (var i = 0; i < ints.length; i++) {
-        holder = s - ints[i];
-        for (var j = 0; j < ints.length; j++) {
-            if (holder == ints[j] && i !== j && i - j > 0) {
-                var indices = i + j;
-                var distance = i - j;
-                film.push([distance, indices, [holder, ints[i]]]);
-            }
-        }
+  for (var i = 0; i < ints.length; i++) {
+    holder = s - ints[i];
+    for (var j = 0; j < ints.length; j++) {
+      if (holder == ints[j] && i !== j && i - j > 0) {
+        var indices = i + j;
+        var distance = i - j;
+        film.push([distance, indices, [holder, ints[i]]]);
+      }
     }
-    if (film.length == 0) {
-        return film[1];
-    } else {
-
-        pair = film.sort(function (a, b) {
-            return a[0] - b[0];
-        });
-        console.log(pair[0][2]);
-        return pair[0][2];
-    }
+  }
+  if (film.length == 0) {
+    return film[1];
+  } else {
+    pair = film.sort(function (a, b) {
+      return a[0] - b[0];
+    });
+    return pair[0][2];
+  }
 };
