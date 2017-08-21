4f1e801c4dbd052239a2f24d785fb0548718ba03
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-08-21 17:52:02 +0530

    seventh week

--- 7 kyu/Without the letter E.js
+++ 7 kyu/Without the letter E.js
@@ -1,3 +1,10 @@
+// option 1
 function findE(str) {
-	return str.match(/e/ig).length;
+	return (str === null) ? null : (!str.length ? '' : ((str.toLowerCase().indexOf('e') > -1) ? String(str.match(/e/ig).length) : 'There is no "e".'));
+}
+
+// option 2
+function findE(str) {
+	const result = (str.match(/e/ig) || []).length;
+	return result ? (''+result) : (str ? 'There is no "e".' : str);
 }
