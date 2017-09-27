b6db876c40a8150810a6fd539dff11b522c5e73a
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-09-27 17:28:32 +0530

    tenth week

--- 4 kyu/Sum Strings as Numbers.js
+++ 4 kyu/Sum Strings as Numbers.js
@@ -1,14 +1,13 @@
-function sumStrings(a, b){
+function sumStrings(a, b) {
   // reversing test strings
   a = a.split('').reverse();
   b = b.split('').reverse();
 
-  var i, remainder = 0;
-  var i = 0;
+  var i = 0, remainder = 0;
   var sumofDigits = [];
 
   // looping till maximum number of digits between two numbers and remainder is not '0'
-  while(i < a.length || i < b.length || remainder != 0){
+  while (i < a.length || i < b.length || remainder != 0) {
 
     // saving current a[i] and b[i] to aDigit and bDigit respectively
     var aDigit = i < a.length ? parseInt(a[i]) : 0;
@@ -23,7 +22,7 @@ function sumStrings(a, b){
   sumofDigits.reverse();
 
   // test case if both the numbers are '0'
-  while(sumofDigits[0] == '0'){
+  while (sumofDigits[0] == '0') {
     sumofDigits.shift();
   }
 
