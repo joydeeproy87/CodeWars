ceebab0cf7f7f19f8a1ca8ebe7afb4eaee1f3075
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-09-27 23:59:02 +0530

    tenth week

--- 6 kyu/Consecutive strings.js
+++ 6 kyu/Consecutive strings.js
@@ -5,6 +5,34 @@
 // n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 
 
+// option 1
 function longestConsec(strarr, k) {
-	return strarr.sort((x, y) => y.length - x.length).splice(0, k).join('');
+	const n = strarr.length;
+	let str = '';
+	let temp = '';
+
+	if (n === 0 || k > n || k <= 0) {
+		return '';
+	} else {
+		for (let i = 0; i < n; i++) {
+			// saving the longest string consisting of k consecutive strings with in 'temp' variable
+			temp = strarr.slice(i, i + k).join('');
+			// checking if 'temp' value is the largest consecutive string
+			if (temp.length > str.length) {
+				str = temp;
+			}
+		}
+	}
+	return str;
+}
+
+
+// option 2 - more optimized
+function longestConsec(strarr, k) {
+	if (strarr === [] || k > strarr.length || k <= 0) {
+		return '';
+	}
+	return strarr
+		.map((value, index) => strarr.slice(index, index + k).join(''))
+		.reduce((longest, current) => current.length > longest.length ? current : longest);
 }
