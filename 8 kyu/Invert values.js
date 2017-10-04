13e65e582c50c30a1e038e3b66b496e86918509f
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-10-04 17:16:52 +0530

    eleventh week

--- 8 kyu/Invert values.js
+++ 8 kyu/Invert values.js
@@ -1,5 +1,12 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+//
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers.
+//
+
+
 function invert(array) {
-  return array.map(function(n) {
-    return (n === 0) ? n : -n;
-  })
+  return array.map(n => n === 0 ? n : -n);
 }
