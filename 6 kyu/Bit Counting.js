cc811c82064e8c096c0263101fb179222b8c6014
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-10-04 14:50:19 +0530

    eleventh week

--- 6 kyu/Bit Counting.js
+++ 6 kyu/Bit Counting.js
@@ -3,13 +3,13 @@
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 
 // option 1
-const countBits = n => {
+const countBits = function countBits(n) {
 	let binary = parseInt(n, 10).toString(2);
 	binary = binary.match(/1/g) || 0;
-  return binary.length || 0;
+	return binary.length || 0;
 };
 
 // option 2
-const countBits = n => {
+const countBits = function countBits(n) {
 	return n.toString(2).split('0').join('').length;
 };
