6596a6e795f83d7e707985f3dc130415fb968e8f
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-10-25 12:52:01 +0530

    thirteenth week

---
+++ 8 kyu/Multiply.ts
@@ -0,0 +1,5 @@
+// The code does not execute properly. Try to figure out why.
+
+export function multiply(a: number, b: number): number {
+   return a * b;
+}
---
+++ 8 kyu/Remove First and Last Character.ts
@@ -0,0 +1,11 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, except in C, where, to keep the difficulty at the level of the kata, you are given two parameters, the first a buffer with length exactly the same as the second parameter, the original string. You don't have to worry with strings with less than two characters.
+
+// option 1
+export function removeChar(str: string): string {
+   return str.substr(1).slice(0, -1);
+}
+
+// option 2
+export function removeChar(str: string): string {
+   return str.slice(1, -1);
+}
