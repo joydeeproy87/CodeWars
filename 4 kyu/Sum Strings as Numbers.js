fa7c0a637d1e7a6f96705806d4386517288b0611
joydeeproy87 <mail2joydeep.roy@gmail.com>
2018-01-22 12:37:21 +0530

    sixteenth week

--- 4 kyu/Sum Strings as Numbers.js
+++ 4 kyu/Sum Strings as Numbers.js
@@ -1,31 +1,31 @@
 function sumStrings(a, b) {
-  // reversing test strings
-  a = a.split('').reverse();
-  b = b.split('').reverse();
+	// reversing test strings
+	a = a.split('').reverse();
+	b = b.split('').reverse();
 
-  let i = 0;
-  let remainder = 0;
-  const sumofDigits = [];
+	let i = 0;
+	let remainder = 0;
+	const sumofDigits = [];
 
-  // looping till maximum number of digits between two numbers and remainder is not '0'
-  while (i < a.length || i < b.length || remainder != 0) {
+	// looping till maximum number of digits between two numbers and remainder is not '0'
+	while (i < a.length || i < b.length || remainder != 0) {
 
-    // saving current a[i] and b[i] to aDigit and bDigit respectively
-    let aDigit = i < a.length ? parseInt(a[i]) : 0;
-    let bDigit = i < b.length ? parseInt(b[i]) : 0;
+		// saving current a[i] and b[i] to aDigit and bDigit respectively
+		let aDigit = i < a.length ? parseInt(a[i]) : 0;
+		let bDigit = i < b.length ? parseInt(b[i]) : 0;
 
-    let digitSum = aDigit + bDigit + remainder;
-    sumofDigits.push((digitSum % 10).toString());
-    remainder = Math.floor(digitSum / 10);
-    i++;
-  }
+		let digitSum = aDigit + bDigit + remainder;
+		sumofDigits.push((digitSum % 10).toString());
+		remainder = Math.floor(digitSum / 10);
+		i++;
+	}
 
-  sumofDigits.reverse();
+	sumofDigits.reverse();
 
-  // test case if both the numbers are '0'
-  while (sumofDigits[0] == '0') {
-    sumofDigits.shift();
-  }
+	// test case if both the numbers are '0'
+	while (sumofDigits[0] == '0') {
+		sumofDigits.shift();
+	}
 
-  return sumofDigits.join('');
+	return sumofDigits.join('');
 }
--- 5 kyu/Sum of Pairs.js
+++ 5 kyu/Sum of Pairs.js
@@ -1,12 +1,12 @@
 var sum_pairs = function sum_pairs(ints, s) {
-  var seen = {};
-  var i = 0;
-  var length = ints.length;
+	var seen = {};
+	var i = 0;
+	var length = ints.length;
 
-  for (i; i < length; ++i) {
-    if (seen[s - ints[i]]) {
-      return [s - ints[i], ints[i]];
-    }
-    seen[ints[i]] = true;
-  }
+	for (i; i < length; ++i) {
+		if (seen[s - ints[i]]) {
+			return [s - ints[i], ints[i]];
+		}
+		seen[ints[i]] = true;
+	}
 };
