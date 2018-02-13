dd69164d1f94a00e1963144c7596acccb45742de
joydeeproy87 <mail2joydeep.roy@gmail.com>
2018-02-13 17:03:29 +0530

    seventeenth week

--- 7 kyu/Beginner Series #3 Sum of Numbers.js
+++ 7 kyu/Beginner Series #3 Sum of Numbers.js
@@ -12,6 +12,18 @@
 
 // option 1
 function GetSum(a, b) {
+	const max = Math.max(a, b);
+	let current = Math.min(a, b);
+	let sum = 0;
+	while (current <= max) {
+		sum += current;
+		current++;
+	}
+	return a === b ? a : sum;
+}
+
+// option 2
+function GetSum(a, b) {
 	let total = 0;
 	if (a === b) {
 		return a;
@@ -28,16 +40,4 @@ function GetSum(a, b) {
 		}
 		return total;
 	}
-}
-
-// option 2
-function GetSum(a, b) {
-	const max = Math.max(a, b);
-	let current = Math.min(a, b);
-	let sum = 0;
-	while (current <= max) {
-		sum += current;
-		current++;
-	}
-	return a === b ? a : sum;
 }
