0953ffbff9575a95422ccfbba87f10086b2b846d
joydeeproy87 <mail2joydeep.roy@gmail.com>
2018-02-13 17:23:07 +0530

    seventeenth week

--- 6 kyu/Equal Sides Of An Array.js
+++ 6 kyu/Equal Sides Of An Array.js
@@ -32,4 +32,18 @@
 function findEvenIndex(arr) {
 	let sum = 0,
 		leftSum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+
+	for (let i = 0; i < arr.length; i++) {
+		sum -= arr[i];
+		if (leftSum === sum) {
+			return i;
+		}
+		leftSum += arr[i];
+	}
+
+	return -1;
 }
